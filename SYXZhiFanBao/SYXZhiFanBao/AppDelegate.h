f2bcb6b9bc42d0517042e83beca48ed6d7cf0676
admin <admin@admindemacbook-pro.local>
2016-08-07 19:27:15 +0800

    Init version

---
+++ SYXZhiFanBao/SYXZhiFanBao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYXZhiFanBao
+//
+//  Created by admin on 16/8/7.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYXZhiFanBao/SYXZhiFanBao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYXZhiFanBao
+//
+//  Created by admin on 16/8/7.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
